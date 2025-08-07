5218ba14fa4c6c9c767e5acf1691f666c2f6fddf
Vinícius Gonçalves Cordeiro <cerradogstudio.viniciuscordeiro@gmail.com>
2025-08-07 02:19:39 -0300

    Update Docker setup: modify composer install command for non-interactive mode, enhance .dockerignore to exclude additional directories, and add index.php and info.php as application entry points.

---
+++ src/public/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file index.php 
+ * Application entrypoint file for the Docker template 
+ * @package vinicius-g-cordeiro/docker-template
+ * @version 0.0.1
+ * @copyright 2025 Vinícius Gonçalves Cordeiro <vinicordeirogo@gmail.com>
+ * @link https://github.com/vinicius-g-cordeiro/docker-template
+ * @link https://linkedin.com/in/vinicordeirox 
+ */
+
+echo "<!DOCTYPE html>
+        <html>
+            <head>
+                <title>Docker Template</title>
+            </head>
+            <body>
+                <h1>Welcome to Docker Template</h1>
+                <footer>
+                    <p>&copy; Copyright, All rights reserved  (2025 Vinícius Gonçalves Cordeiro)</p>
+                </footer>
+            </body>
+        </html>";
+
+exit(0);
---
+++ src/public/info.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file info.php 
+ * @package vinicius-g-cordeiro/docker-template
+ * @version 0.0.1
+ * @copyright 2025 Vinícius Gonçalves Cordeiro <vinicordeirogo@gmail.com>
+ * @link https://github.com/vinicius-g-cordeiro/docker-template
+ * @link https://linkedin.com/in/vinicordeirox
+ */
+
+phpinfo();
